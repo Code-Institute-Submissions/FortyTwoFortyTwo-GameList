8ee4d17922bc00a39b7918a1e543162fbb892027
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2021-08-16 23:29:10 +0100

    Change edit button to save on pressed
    To allow save button be selected during edit

--- static/js/main.js
+++ static/js/main.js
@@ -6,4 +6,11 @@ $(document).on("click", ".game-edit", function ()
 	//Allow input editable
 	$("input, textarea").removeAttr("readonly");
 	$("input, textarea").addClass("color-orange");
+	
+	//Change edit button to save
+	$(".game-edit").text("Save");
+	$(".game-edit").addClass("color-green");
+	$(".game-edit").removeClass("color-orange");
+	$(".game-edit").addClass("game-save");
+	$(".game-edit").removeClass("game-edit");
 });
